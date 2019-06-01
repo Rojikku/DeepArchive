1ca11bee461e7c594b1589619b92e7759be2b934
Rojikku <RojikkuNoKami@gmail.com>
2019-06-01 14:11:45 -0400

    Basic Database Population

--- Archive.py
+++ Archive.py
@@ -5,27 +5,45 @@ import os #Needed to chdir
 #Variables
 dbl = '.da/db.sqlite' #Database Location
 tbln = 'core' #Table name
+idc='Name' #Identifying Column
+dir = 'C:\\Users\\Rojikku\\Documents\\Library\\PDF' #Directory in use TEMPORARY
+scan_exceptions = ['.da'] #Files to ignore when scanning
 
-#Go to relevant  directory
-#Must add direcotry selecting functionality
-os.chdir('C:\\Users\\Rojikku\\Documents\\Library\\PDF')
+#Functions Section
+def Insert(Col, Val):
+    c.execute("INSERT INTO {tn} ({col}) VALUES ('{val}')"\
+        .format(tn=tbln, col=Col, val=Val))
 
-#def Scan(): #Function for scanning
+def Scan(): #Function for scanning
+    for filename in os.listdir(dir):
+        if filename in scan_exceptions: #Skip adding exceptions to the database
+            continue
+        print("Adding {fn} to database".format(fn=filename))
+        Insert(idc, filename)
 
 def Initialize(): #Function to create database foundations
     c.execute('CREATE TABLE {tn} ({nf} {ft})'\
-        .format(tn=tbln, nf='Name', ft='TEXT'))
+        .format(tn=tbln, nf=idc, ft='TEXT'))
     conn.commit() #Save
 
+#Init
+#Go to relevant  directory
+#Must add direcotry selecting functionality
+os.chdir(dir)
 
 exists = os.path.isfile(dbl) #Check if DB exists
 if exists: #Connect if so
     conn = sqlite3.connect(dbl)
     c = conn.cursor()
 else: #Create and initialize if not
-    os.mkdir('.da')
-    conn = sqlite3.connect(dbl)
+    os.mkdir('.da') #Make DB folder
+    conn = sqlite3.connect(dbl) #Create DB File
     c = conn.cursor()
     Initialize()
 
+#Main
+Scan()
+
+#Ends
+conn.commit()
 conn.close() #Close connection
