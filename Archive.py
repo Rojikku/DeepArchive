adbed29235f15a554cfa5f69055194c91d7c4fac
Rojikku <Rojikku@users.noreply.github.com>
2019-06-24 20:45:33 -0400

    Switched to Qt5

--- Archive.py
+++ Archive.py
@@ -2,8 +2,8 @@
 import sqlite3 #Database
 import os #Needed to chdir
 import sys #Allow Arguments
-from tkinter import filedialog #Select Folders
-from tkinter import * #UI
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QMainWindow, QAction, qApp, QLabel, QFileDialog, QGridLayout, QPushButton, QStackedWidget
+from PyQt5.QtGui import QIcon
 
 #Variables
 dbfolder = '.da' #Name of Database Folder
@@ -13,8 +13,6 @@ tbln = 'core' #Table name
 idc='FileName' #Identifying Column
 scan_exceptions = ['.da', 'db.sqlite'] #Files to ignore when scanning
 db_status = 0 #Current DB Status
-conn = None #Set a default
-c = None #Set a default
 
 #Functions Section
 def Close(): #In case I add to it later
@@ -57,90 +55,94 @@ def Initialize(): #Function to create database foundations
     print("Done!")
 
 #UI Functions
-class DAUI(Frame):
-    def __init__(self, master):
-        Frame.__init__(self, master)
-        self.master = master
+class DAUI(QMainWindow):
+    def __init__(self):
+        super().__init__() #Load Parent's Init
 
+        self.initUI()
+
+
+    def initUI(self):
         #Settings
-        self.master = master
-        master.title("Deep Archive: The File Library Manager")
+        self.setGeometry(300, 300, 300, 220)
+        self.center()
+        self.setWindowTitle("Deep Archive: The File Library Manager")
 
         #Menu Bar
-        # creating a menu instance
-        menu = Menu(self.master)
-        self.master.config(menu=menu)
+        menu = self.menuBar()
+        fileMenu = menu.addMenu('&File')
 
-        # Declare File Menu
-        file = Menu(menu)
-        file.add_command(label="Open Library", command=self.browseLib)
-        file.add_command(label="Exit", command=master.quit)
-
-        # create the file object)
-        edit = Menu(menu)
-        edit.add_command(label="Undo")
-
-        #Add Entries to Menu Bar
-        menu.add_cascade(label="File", menu=file)
-        menu.add_cascade(label="Edit", menu=edit)
-
-        #String Data
-        global Lbl1
-        Lbl1 = StringVar()
-        Lbl1.set("Please select library directory!")
-        self.label = Label(master, textvariable=Lbl1)
-        self.label.grid(row=0, column=2, padx=(10, 10), pady=(10, 10))
+        #File Menu
+        #Exit
+        exitAct = QAction(QIcon('exit.png'), '&Exit', self)
+        exitAct.setShortcut('Ctrl+Q')
+        exitAct.setStatusTip('Exit application')
+        exitAct.triggered.connect(qApp.quit)
+        #Open library
+        openLibrary = QAction(QIcon('open.png'), '&Open Library...', self)
+        openLibrary.setShortcut('Ctrl+O')
+        openLibrary.setStatusTip('Open A Library')
+        openLibrary.triggered.connect(self.browseLib)
+        #Organization
+        fileMenu.addAction(openLibrary)
+        fileMenu.addAction(exitAct)
 
 
-        self.center()
+        #Core
+        #Files
+        self.central_widget = QStackedWidget()
+        self.setCentralWidget(self.central_widget)
+        FUI = FileUI()
+        self.central_widget.addWidget(FUI)
 
-    def populate(self): #Function for adding menu entries from Database
-        global conn, c
-        files=[]
-        buttonList=[]
-        for row in c.execute('SELECT * FROM {tn} ORDER BY {ord}'.format(tn=tbln, ord=idc)):
-            files.append(row)
-            for i in range(len(files)):
-                name = files[i][0]
-                print("File loaded from DB: " + name)
-                newButton = Button(root, text=name, command = lambda j=name: openFile(j))
-                buttonList.append(newButton)
-        for i,x in enumerate(buttonList):
-            x.grid(row=2, column=i, padx=(10, 10), pady=(10, 10))
-
+        self.show()
 
     def browseLib(self): #Select Library Location
         # Allow user to select a directory and store it in global var
         # called dir
         global dir
-        filename = filedialog.askdirectory(parent=root, title="Please Select Library Folder:")
-        Lbl1.set(filename)
-        dir = str(filename)
+        dir = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        self.setWindowTitle(dir + " - Deep Archive")
         print(dir)
         Initialize()
-        self.populate()
+        FileUI.populate(self)
 
     def center(self): #Function to center window
-        # Gets the requested values of the height and widht.
-        windowWidth = root.winfo_reqwidth()
-        windowHeight = root.winfo_reqheight()
-        print("Width",windowWidth,"Height",windowHeight)
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
-        # Gets both half the screen width/height and window width/height
-        positionRight = int(root.winfo_screenwidth()/2 - windowWidth/2)
-        positionDown = int(root.winfo_screenheight()/2 - windowHeight/2)
+class FileUI(QWidget): #Widget for main window pane
+    def __init__(self):
+        super().__init__() #Load Parent's Init
 
-        # Positions the window in the center of the page.
-        root.geometry("+{}+{}".format(positionRight, positionDown))
-        root.geometry("400x300") #I'd like a less ugly default window size
+        self.initUI() #Load UI
 
+    def initUI(self):
+        global grid #Make layout available to populate
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+    def populate(self):
+        global conn, c, grid #Load DB and Layout
+        fileEntries = []
+        fileNames = []
+        positions = [(i,j) for i in range(10) for j in range(4)]
+        for row in c.execute('SELECT * FROM {tn} ORDER BY {ord}'.format(tn=tbln, ord=idc)):
+            fileEntries.append(row)
+        for i in range(len(fileEntries)):
+            name = fileEntries[i][0]
+            print("File loaded from DB: " + name)
+            fileNames.append(name)
+        for position, name in zip(positions, fileNames):
+            button = QPushButton(name)
+            grid.addWidget(button, *position)
 
 #Init
-root = Tk()
-ui = DAUI(root)
-
-#Main
-root.mainloop()
+app = QApplication(sys.argv)
+ui = DAUI()
+sys.exit(app.exec_()) #Load UI
 
 #End
 if db_status == 1:
