537452b3101e9790ed6cb0e064d8ed54abeb9894
Rojikku <Rojikku@users.noreply.github.com>
2020-06-16 13:46:54 -0400

    Better Version Numbers

---
+++ interface/__init__.py
@@ -0,0 +1,6 @@
+"""
+App Version Information
+"""
+__version__ = '0.0.1'
+VERSION = __version__  # alias
+__build__ = ''
---
+++ interface/templatetags/version.py
@@ -0,0 +1,28 @@
+"""
+Give Current Version to templates
+"""
+from os import path
+import subprocess
+from django import template
+import interface
+
+register = template.Library()
+
+
+@register.simple_tag
+def current_version():
+    """
+    Generate Version and Build from GIT
+    """
+    BASE_DIR = path.dirname(path.dirname(path.abspath(__file__)))
+
+    try:
+        build = subprocess.check_output(
+            ["git", "describe", "--tags"],
+            cwd=BASE_DIR).decode('utf-8').strip()
+    except:
+        build = interface.__version__ + "-dev-"
+        build += subprocess.check_output(
+            ["git", "describe", "--tags", "--always"],
+            cwd=BASE_DIR).decode('utf-8').strip()
+    return "v" + build
