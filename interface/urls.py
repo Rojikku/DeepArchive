5efb5639f1dbcce6d5548fd53b44e0545e5c6d93
Rojikku <Rojikku@users.noreply.github.com>
2020-06-16 13:46:42 -0400

    Archive Page Init

--- interface/urls.py
+++ interface/urls.py
@@ -6,5 +6,7 @@ from django.urls import path
 from interface import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("db", views.archive, name="archive"),
+    path("db/<dbname>", views.archiveviewer, name="archiveviewer"),
 ]
--- interface/views.py
+++ interface/views.py
@@ -5,5 +5,29 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
+    """
+    Homepage
+    """
     return render(request, "interface/home.html")
+
+def archive(request):
+    """
+    Archive List
+    """
+    return render(request, "interface/db.html")
+
+def archiveviewer(request, dbname):
+    """
+    Archive page for dbnew, and dbview
+    DBnew: Create an Archive
+    DBview: View an Archive
+    """
+    if dbname is not None:
+        return render(request, "interface/dbview.html",
+                      {
+                          "dbname": dbname,
+                      })
+    else:
+        return render(request, "interface/dbnew.html")
