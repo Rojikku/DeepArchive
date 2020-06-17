33d27b283534bda70aa2db7c8679397c84fdbceb
Rojikku <Rojikku@users.noreply.github.com>
2020-06-17 18:44:17 -0400

    Migrate to Versioneer

--- DeepArchive/__init__.py
+++ DeepArchive/__init__.py
@@ -1,6 +1,8 @@
 """
 App Version Information
 """
-__version__ = '0.0.1'
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
 VERSION = __version__  # alias
-__build__ = ''
--- DeepArchive/templatetags/version.py
+++ DeepArchive/templatetags/version.py
@@ -14,15 +14,4 @@ def current_version():
     """
     Generate Version and Build from GIT
     """
-    BASE_DIR = path.dirname(path.dirname(path.abspath(__file__)))
-
-    try:
-        build = subprocess.check_output(
-            ["git", "describe", "--tags"],
-            cwd=BASE_DIR).decode('utf-8').strip()
-    except:
-        build = DeepArchive.__version__ + "-dev-"
-        build += subprocess.check_output(
-            ["git", "describe", "--tags", "--always"],
-            cwd=BASE_DIR).decode('utf-8').strip()
-    return "v" + build
+    return "v" + DeepArchive.__version__
