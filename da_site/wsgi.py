bbef209e536a9bc50e5d1f683884ee9e8bed1b9d
Rojikku <Rojikku@users.noreply.github.com>
2020-06-17 17:57:19 -0400

    Renamed Django Project

--- da_site/wsgi.py
+++ da_site/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DeepArchive project.
+WSGI config for da_site project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DeepArchive.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'da_site.settings')
 
 application = get_wsgi_application()
